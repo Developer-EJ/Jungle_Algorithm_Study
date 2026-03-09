8daf601665405fa1b291c67994251a2a158a0f09
Developer-EJ <155644012+Developer-EJ@users.noreply.github.com>
2026-03-09 19:55:13 +0900

    week2/problem-sloving 난이도하 문제 해결

--- week2/basic/08_bubble_sort.py
+++ week2/basic/08_bubble_sort.py
@@ -64,19 +64,18 @@ def bubble_sort_optimized(arr):
     """
     n = len(arr)
 
-    for i in range(n):
+    for i in range(n - 1):
         swapped = False  # 교환 발생 여부
 
         # TODO: 내부 반복문과 교환 로직 구현
         # 교환이 발생하면 swapped = True 설정
-        for j in range(n - i):
+        for j in range(n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
-            else:
-                break
         # TODO: 교환이 없으면 이미 정렬된 것이므로 break
-        pass
+        if swapped == False:
+            break
 
     return arr
 
